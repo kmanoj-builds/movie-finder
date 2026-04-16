5062077520be2d6c2e5e0bb4a72536bf35e8ccaa
kmanoj-builds <kmanojat@gmail.com>
2026-04-16 15:55:13 +0530

    v1

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,11 @@
-
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
-import { BrowserRouter as Router } from 'react-router-dom'
+import { createRoot } from 'react-dom/client';
+import './index.css';
+import App from './App.jsx';
+import { BrowserRouter as Router } from 'react-router-dom';
 import 'flowbite';
-
 
 createRoot(document.getElementById('root')).render(
   <Router>
     <App />
-  </Router>,
-)
+  </Router>
+);
